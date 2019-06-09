947409e8b5ff9032dfdc842df3c933d8abafccf0
Pradeep Guptha Ravuri <pradeeprbvs.514@gmail.com>
2019-06-10 02:04:40 +0530

    Completed Enterprise Survey Module

--- enterprise_survey/src/main/scala/com/lumiqai/assessment/enterprisesurvey/EnterpriseSurvey.scala
+++ enterprise_survey/src/main/scala/com/lumiqai/assessment/enterprisesurvey/EnterpriseSurvey.scala
@@ -1,17 +1,32 @@
 package com.lumiqai.assessment.enterprisesurvey
 
-import org.apache.spark.sql.functions.col
 import com.lumiqai.assessment.utils.FilesUtil
+import org.apache.spark.sql.functions.col
 
 object EnterpriseSurvey extends FilesUtil {
 
+  private val appName: String = "enterpriseSurvey"
+
   def main(args: Array[String]): Unit = {
 
-    val df = readCSV("enterpriseSurvey")
+    logger.info("--Application Started--")
 
-    df.printSchema()
+    try {
 
-    df.filter(col("corrupt_record").isNotNull).show(false)
+      val df = readCSV(appName).cache()
+
+      writeParquet(appName, "file", df.filter(col("corrupt_record").isNull))
+      writeParquet(appName, "badData", df.filter(col("corrupt_record").isNotNull))
+
+    } catch {
+      case exception: Exception =>
+        logger.error("Application failed with Error: ", exception)
+        throw exception
+
+    } finally {
+      spark.stop()
+      logger.info("Application terminated")
+    }
 
   }
 
