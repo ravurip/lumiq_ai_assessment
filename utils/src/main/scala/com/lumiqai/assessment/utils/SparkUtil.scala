7b4c48b24a71bec10eb56a3aeb7d0b41551ff1fe
Pradeep Guptha Ravuri <pradeeprbvs.514@gmail.com>
2019-06-10 01:26:43 +0530

    Added logging and exception handling in Spark util

--- utils/src/main/scala/com/lumiqai/assessment/utils/SparkUtil.scala
+++ utils/src/main/scala/com/lumiqai/assessment/utils/SparkUtil.scala
@@ -1,6 +1,7 @@
 package com.lumiqai.assessment.utils
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.SparkException
 import org.apache.log4j.{Level, Logger}
 
 trait SparkUtil extends ConfigReader {
@@ -10,18 +11,26 @@ trait SparkUtil extends ConfigReader {
   lazy val spark: SparkSession = {
     try {
       Logger.getLogger("org").setLevel(Level.ERROR)
+
       val spark = SparkSession
         .builder()
         .master(runMode)
         .getOrCreate()
+
+      logger.info("Successfully initialized spark session")
       spark.conf.set("spark.sql.columnNameOfCorruptRecord", "corrupt_record")
 
       spark
     } catch {
-      case exception: Exception => {
+
+      case exception: SparkException =>
+        logger.error("Error initialising SparkSession. Validate the master mode provided.")
+        throw exception
+
+      case exception: Exception =>
         logger.error("Error initialising SparkSession")
         throw exception
-      }
+
     }
   }
 }
